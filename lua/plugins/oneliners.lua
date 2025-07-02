3fdf3cf60e6e06bdf69e321590a6fa829d043618
bhavesh <bhaveshp.patil.007@gmail.com>
2025-07-02 17:11:05 +0530

    05. rolling back didnt work  --before random dotfile

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -8,48 +8,13 @@ return {
 	end,
     },
 
-    --git vim
+
     {
 	    'tpope/vim-fugitive',
     },
 
     {
-        'williamboman/mason.nvim',
-        opts = {
-            ensure_installed = {
-                "pyright",
-            }
-        }
+        'williamboman/mason.nvim', 
 
-    },
-
-{
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-
-    -- Optional: general on_attach function
-    local on_attach = function(client, bufnr)
-      local opts = { buffer = bufnr }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    end
-
-    -- Setup Python LSP
-    lspconfig.pyright.setup({
-      on_attach = on_attach,
-    })
-
-end
     }
-    --lsp-config
---    {
---        "neovim/nvim-lspconfig",
---        custom = function()
---            require "config.lspconfig"
---        end,
-
---    }
-} --return
+}
