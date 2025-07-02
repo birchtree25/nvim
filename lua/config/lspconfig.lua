b901fa1d1dfff7ed719fabc5dd6f8b1da7766fb3
bhavesh <bhaveshp.patil.007@gmail.com>
2025-07-02 17:06:52 +0530

    03. before random dotfile

---
+++ lua/config/lspconfig.lua
@@ -0,0 +1,16 @@
+local config = require("plugins.oneliners")
+
+local on_attach = config.on_attach
+local  capabilities = config.capabilites
+
+local lspconfig = reqiure("lspconfig")
+
+lspconfig.pyright.setup({
+    on_attach = on_attach,
+    capabilites = capabilities,
+    filetypes = {"python"},
+
+})
+
+
+
--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -8,13 +8,48 @@ return {
 	end,
     },
 
-
+    --git vim
     {
 	    'tpope/vim-fugitive',
     },
 
     {
-        'williamboman/mason.nvim', 
+        'williamboman/mason.nvim',
+        opts = {
+            ensure_installed = {
+                "pyright",
+            }
+        }
 
+    },
+
+{
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+
+    -- Optional: general on_attach function
+    local on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    end
+
+    -- Setup Python LSP
+    lspconfig.pyright.setup({
+      on_attach = on_attach,
+    })
+
+end
     }
-}
+    --lsp-config
+--    {
+--        "neovim/nvim-lspconfig",
+--        custom = function()
+--            require "config.lspconfig"
+--        end,
+
+--    }
+} --return
